daa080de699977b68ff586cbd88633b1c6814d3c
Mohammad Mudallal <mohammad.mudallal@ara-technologies.com>
2026-03-11 17:33:14 +0200

    Added full results.json file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.post("/process-message", async (req, res) => {
   try {
-    const classification = await processMessage(req.body);
+    processMessage(req.body);
     return res.status(200).json({ response: `Your request was processed` });
   } catch(e){
     console.error("Error processing message:", e);
